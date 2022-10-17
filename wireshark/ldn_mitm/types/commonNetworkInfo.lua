72f8e0d5822a9c544da8aab17980cd53ebac58c4
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-10-17 02:21:44 +0200

    wireshark: Decode SSID in a better format for ldn_mitm

--- wireshark/ldn_mitm/types/commonNetworkInfo.lua
+++ wireshark/ldn_mitm/types/commonNetworkInfo.lua
@@ -1,13 +1,14 @@
 -- Proto fields
 local f_bssid = ProtoField.ether("ldn_mitm.common.bssid", "BSSID")
-local f_ssid = ProtoField.bytes("ldn_mitm.common.ssid", "SSID", base.NONE)
+local f_ssid_length = ProtoField.uint8("ldn_mitm.common.ssid_length", "SSID length")
+local f_ssid = ProtoField.stringz("ldn_mitm.common.ssid", "SSID", base.NONE)
 local f_channel = ProtoField.int16("ldn_mitm.common.channel", "Channel", base.DEC)
 local f_link_level = ProtoField.int8("ldn_mitm.common.link_level", "Link level", base.DEC)
 local f_network_type = ProtoField.uint8("ldn_mitm.common.network_type", "Network type", base.DEC)
 
 -- Proto
 local p_common_netinfo = Proto("ldn_mitm.common", "Common network info")
-p_common_netinfo.fields = {f_bssid, f_ssid, f_channel, f_link_level, f_network_type}
+p_common_netinfo.fields = {f_bssid, f_ssid_length, f_ssid, f_channel, f_link_level, f_network_type}
 
 -- Dissector
 function p_common_netinfo.dissector(buffer, pinfo, tree)
@@ -18,8 +19,8 @@ function p_common_netinfo.dissector(buffer, pinfo, tree)
 
     local subtree = tree:add(p_common_netinfo, buffer(0, 48), "Common network info")
     subtree:add_le(f_bssid, buffer(0, 6))
-    -- SSID: length(1) + name(32 + 1)
-    subtree:add_le(f_ssid, buffer(6, 34))
+    subtree:add_le(f_ssid_length, buffer(6, 1))
+    subtree:add_le(f_ssid, buffer(7, 33))
     subtree:add_le(f_channel, buffer(40, 2))
     subtree:add_le(f_link_level, buffer(42, 1))
     subtree:add_le(f_network_type, buffer(43, 1))
